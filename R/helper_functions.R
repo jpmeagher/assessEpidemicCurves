a93a2742076c36f07366b1d2b1ad018ef04e1aba
jpmeagher <J.Meagher@warwick.ac.uk>
2021-05-26 21:37:44 +0100

    add gamma helper function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,6 +1,25 @@
-convert_gamma_moments <- function(expectation, standard_deviation){
+#' Convert gamma moments
+#'
+#' Convert the first two moments of a gamma distribution to shape and rate
+#' parameters.
+#'
+#' @param expectation A scalar. The expected value of the Gamma random variable.
+#' @param standard_deviation A scalar. The standard deviation of the Gamma
+#'   random variable.
+#' @param perform_checks Logical. Should function arguments be checked to ensure
+#'   they satisfy any constraints.
+#'
+#' @return A named vector of length 2. The shape and rate for the Gamma random
+#'   variable.
+#' @export
+#' @examples
+#' convert_gamma_moments(5, 1)
+convert_gamma_moments <- function(expectation, standard_deviation, perform_checks = TRUE){
+  if (perform_checks) {
+    checkmate::assert_number(expectation, lower = 0, finite = TRUE)
+    checkmate::assert_number(standard_deviation, lower = 0, finite = TRUE)
+  }
   shape <- (expectation / standard_deviation)^2
-  rate <- expectation / standard_deviation^2
-
+  rate <- expectation / (standard_deviation^2)
   c("shape" = shape, "rate" = rate)
 }
