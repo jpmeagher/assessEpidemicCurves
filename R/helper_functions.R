1b9862bea0d14a2372ee0edeae7ef3718a6ca1d8
jpmeagher <J.Meagher@warwick.ac.uk>
2021-05-27 10:03:54 +0100

    inverse gamma helper function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -3,13 +3,13 @@
 #' Convert the first two moments of a gamma distribution to shape and rate
 #' parameters.
 #'
-#' @param expectation A scalar. The expected value of the Gamma random variable.
-#' @param standard_deviation A scalar. The standard deviation of the Gamma
+#' @param expectation A scalar. The expected value of the gamma random variable.
+#' @param standard_deviation A scalar. The standard deviation of the gamma
 #'   random variable.
 #' @param perform_checks Logical. Should function arguments be checked to ensure
 #'   they satisfy any constraints.
 #'
-#' @return A named vector of length 2. The shape and rate for the Gamma random
+#' @return A named vector of length 2. The shape and rate for the gamma random
 #'   variable.
 #' @export
 #' @examples
@@ -21,5 +21,36 @@ convert_gamma_moments <- function(expectation, standard_deviation, perform_check
   }
   shape <- (expectation / standard_deviation)^2
   rate <- expectation / (standard_deviation^2)
+  c("shape" = shape, "rate" = rate)
+}
+
+#' Convert inverse-gamma moments
+#'
+#' Convert the first two moments of an inverse-gamma distribution to shape and rate
+#' parameters.
+#'
+#' @param expectation A scalar. The expected value of the inverse-gamma random variable.
+#' @param standard_deviation A scalar. The standard deviation of the inverse-gamma
+#'   random variable.
+#' @param perform_checks Logical. Should function arguments be checked to ensure
+#'   they satisfy any constraints.
+#'
+#' @returnA named vector of length 2. The shape and rate for the inverse-gamma random
+#'   variable.
+#' @export
+#'
+#' @examples
+#' convert_inverse_gamma_moments(5, 1)
+convert_inverse_gamma_moments <- function(expectation, standard_deviation, perform_checks = TRUE){
+  if (perform_checks) {
+    checkmate::assert_number(expectation, lower = 0, finite = TRUE)
+    checkmate::assert_number(standard_deviation, lower = 0, finite = TRUE)
+  }
+  shape <- (expectation / standard_deviation)^2 + 2
+  rate <- expectation * (shape - 1)
+  if (perform_checks) {
+    checkmate::assert_number(shape, lower = 0, finite = TRUE)
+    checkmate::assert_number(rate, lower = 0, finite = TRUE)
+  }
   c("shape" = shape, "rate" = rate)
 }
--- tests/testthat/test-helper_functions.R
+++ tests/testthat/test-helper_functions.R
@@ -10,3 +10,18 @@ test_that("Gamma converter works", {
   expect_equal(convert_gamma_moments(expectation = mu, standard_deviation = sigma)[1], alpha, ignore_attr = TRUE)
   expect_equal(convert_gamma_moments(expectation = mu, standard_deviation = sigma)[2], beta, ignore_attr = TRUE)
 })
+
+test_that("Inverse-gamma converter works", {
+  alpha <- 20
+  beta <- 10
+  mu <- beta / (alpha - 1)
+  sigma <- sqrt((beta^2) / ((alpha - 1)^2 * (alpha - 2)))
+
+  expect_error(convert_inverse_gamma_moments(expectation = -1, standard_deviation = 1))
+  expect_error(convert_inverse_gamma_moments(expectation = 1, standard_deviation = -1))
+  expect_named(convert_inverse_gamma_moments(expectation = mu, standard_deviation = sigma), expected = c("shape", "rate"))
+  expect_equal(convert_inverse_gamma_moments(expectation = mu, standard_deviation = sigma)[1], alpha, ignore_attr = TRUE)
+  expect_equal(convert_inverse_gamma_moments(expectation = mu, standard_deviation = sigma)[2], beta, ignore_attr = TRUE)
+
+  convert_inverse_gamma_moments(expectation = mu, standard_deviation = sigma)
+})
