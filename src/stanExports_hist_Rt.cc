9a80111836345d9ce16b4b072a5f362d9c3140c7
jpmeagher <josephmeagher07@gmail.com>
2024-04-07 16:16:40 +0100

    build and install package

--- src/stanExports_hist_Rt.cc
+++ src/stanExports_hist_Rt.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4hist_Rt_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_hist_Rt")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_hist_Rt")
 
     .constructor<SEXP,SEXP,SEXP>()
 
